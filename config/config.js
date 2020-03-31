b0ecad207fe88c2a9c3b870b94fcf50bac41a7c3
NelushX <hjalle1990@hotmail.com>
2020-03-31 11:22:38 +0200

    fix

--- config/config.js
+++ config/config.js
@@ -1,8 +1,6 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
-// process.env.varnamnet
 
 const config = {
-    // databaseUrl: "mongodb+srv://HannaGoldhammer:Hanna123@cluster0-kimns.mongodb.net/lasseslakrits?retryWrites=true&w=majority"
     databaseUrl: process.env.DATABASE,
     mail: process.env.MAIL
 }
--- router/router.js
+++ router/router.js
@@ -218,8 +218,8 @@ router.get("/deleteWishlist/:id", verifyToken, async (req, res) => {
 //     })
 
 router.get("/checkout", verifyToken, async (req, res) => {
-    const user = await User.findOne({ _id: req.user.user._id }).populate("order.orderId");
-    res.render("public/checkout", { token: req.cookies.jsonwebtoken, user, title: "Önskelista - Lasses" });
+    const user = await User.findOne({ _id: req.user.user._id }).populate("wishlist.candyId");
+    res.render("public/checkout", { token: req.cookies.jsonwebtoken, user, title: "Kassa - Lasses" });
 });
 
 router.get("/checkout/:id", verifyToken, async (req, res) => {
